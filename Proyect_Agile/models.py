8a7bb43d893d8d960c86619c7a5c45b64e94a953
Alejandro Marin <alexword1893@gmail.com>
2022-08-26 20:54:10 -0400

    se agregó el boton de navegacion

--- Proyect_Agile/models.py
+++ Proyect_Agile/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=30)
+    miembros = models.CharField(max_length=20)
