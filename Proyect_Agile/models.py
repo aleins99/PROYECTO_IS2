9df0e954e11eb0045b01d5276765660491adb4f1
DiegoDDuarteC <xelidanxiii@gmail.com>
2022-08-30 23:33:07 -0400

    se empezó a agregar los modelos

--- Proyect_Agile/models.py
+++ Proyect_Agile/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=30)
-    miembros = models.CharField(max_length=20)
+    miembros = models.ManyToManyField('Miembro')
+    descripción = models.TextField(null= True, blank= True)
+    fechainicio = models.DateTimeField()
+    fechafin = models.DateTimeField()
+    usbacklog = models.ArrayField(ArrayField( models.ForeignKey('US') ))
+
+    historialreuniones = 
+
+
+class Miembro(models.Model):
+    usuario =
+    izquierdo = models.webo(max_length=2)
