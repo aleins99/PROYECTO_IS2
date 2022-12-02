36d648edbee67498c7a11914e2496cb7c880ea23
aleins99 <alexword1893@gmail.com>
2022-12-01 22:46:17 -0300

    cambios al script

--- script.sh
+++ script.sh
@@ -6,28 +6,6 @@
 echo "Ejcutando script"
 
 cd ~
-cd Desktop/IS2/PROYECTO_IS2/
-
-if [ "$1" = "-t" ]; then
-
-	git fetch
-	#git checkout $2
-
-	DB_NAME="producciondb"
-	DB_FILE_NAME="desarrollo.sql"
-
-	#psql -h localhost -p 5432  -U usuario $DB_NAME < $DB_FILE_NAME	 # poblar BD
-
-	python3 manage.py runserver
-fi
-#!/bin/bash
-
-# ./script.sh -t nombre-del-tag
-# ./script.sh -t iteracion5
-
-echo "Ejcutando script"
-
-cd ~
 cd Desktop/Backend/PROYECTO_IS2/
 DB_NAME="producciondb"
 DB_FILE_NAME="desarrollo.sql"
