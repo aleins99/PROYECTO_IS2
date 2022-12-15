97cb1ca959edfb31882c821d3b9aa240ee3ae6f9
agile <alexword1893@gmail.com>
2022-12-15 04:36:57 +0000

    staticos

---
+++ Proyect_Agile/static/js/script.b3d92f0e6eab.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById('navbar');
+const right = document.getElementById('right-screen');
+    console.log('hola')
+
+setTimeout(function (){
+    $('.alert-block').fadeOut('slow')
+}, 3000)
